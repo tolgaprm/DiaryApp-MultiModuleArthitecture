31cdc57baccd7049e4a3e187b51916d0fff61d58
Tolga Pirim <tolgapirim25@gmail.com>
2023-03-10 17:36:04 +0300

    Implemented One-Tap Sign in with Google

--- app/src/main/java/com/prmto/diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/prmto/diaryapp/navigation/NavGraph.kt
@@ -1,7 +1,6 @@
 package com.prmto.diaryapp.navigation
 
 import android.annotation.SuppressLint
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -10,8 +9,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.prmto.diaryapp.presantation.screens.auth.AuthenticationScreen
-import com.prmto.diaryapp.util.Constants
 import com.prmto.diaryapp.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
+import com.stevdzasan.messagebar.rememberMessageBarState
+import com.stevdzasan.onetap.rememberOneTapSignInState
 
 @Composable
 fun SetupNavGraph(startDestinationScreen:Screen, navController: NavHostController) {
@@ -26,10 +26,15 @@ fun SetupNavGraph(startDestinationScreen:Screen, navController: NavHostControlle
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 fun NavGraphBuilder.authenticationRoute(){
     composable(Screen.Authentication.route){
-        AuthenticationScreen(
-            loadingState = false,
-            onButtonClicked = {
 
+        val oneTapState = rememberOneTapSignInState()
+        val messageBarState = rememberMessageBarState()
+        AuthenticationScreen(
+            loadingState = oneTapState.opened,
+            oneTapSignInState = oneTapState,
+            messageBarState = messageBarState,
+            onButtonClicked = {
+                oneTapState.open()
             }
         )
     }
--- app/src/main/java/com/prmto/diaryapp/presantation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/prmto/diaryapp/presantation/screens/auth/AuthenticationScreen.kt
@@ -1,23 +1,48 @@
 package com.prmto.diaryapp.presantation.screens.auth
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.prmto.diaryapp.BuildConfig
+import com.prmto.diaryapp.util.Constants
+import com.prmto.diaryapp.util.Constants.CLIENT_ID
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.MessageBarState
+import com.stevdzasan.onetap.OneTapSignInState
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
+import java.lang.Exception
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AuthenticationScreen(
     loadingState: Boolean,
+    oneTapSignInState: OneTapSignInState,
+    messageBarState: MessageBarState,
     onButtonClicked: () -> Unit
 ) {
     Scaffold(
         content = {
-            AuthenticationContent(
-                loadingState = loadingState,
-                onButtonClicked = onButtonClicked
-            )
+            ContentWithMessageBar(messageBarState =messageBarState ) {
+                AuthenticationContent(
+                    loadingState = loadingState,
+                    onButtonClicked = onButtonClicked
+                )
+            }
         }
     )
+
+    OneTapSignInWithGoogle(
+        state = oneTapSignInState,
+        clientId = CLIENT_ID,
+        onTokenIdReceived = {tokenId->
+            Log.d("auth",tokenId)
+            messageBarState.addSuccess("Successfully authenticated")
+        },
+        onDialogDismissed = {message->
+            Log.d("auth",message)
+            messageBarState.addError(Exception(message))
+        })
 }
