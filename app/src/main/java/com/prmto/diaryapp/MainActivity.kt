8db305904cfbe8fd0822303621103134870ba971
Tolga Pirim <tolgapirim25@gmail.com>
2023-03-10 14:25:06 +0300

    Implemented Splash Screen

--- app/src/main/java/com/prmto/diaryapp/MainActivity.kt
+++ app/src/main/java/com/prmto/diaryapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.prmto.diaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.prmto.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryAppTheme {
 
